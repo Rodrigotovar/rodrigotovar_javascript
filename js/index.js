f90b6449f5101b9379e8f61126038e521706261e
Rodrigotovar <rodrigotovarpaura@gmail.com>
2024-07-29 15:29:10 -0600

    0.0.3
    Calcular el precio total de productos seleccionados

--- js/index.js
+++ js/index.js
@@ -1,5 +1,51 @@
-const producto1 = Playera1;
-const producto2 = Playera2;
-const producto3 = Playera3;
+// aquí definimos los precios de los productos
 
-const escogerProducto = prompt("Escoge un producto")
+const PRECIO_PRODUCTO_1 = 199;
+const PRECIO_PRODUCTO_2 = 299;
+const PRECIO_PRODUCTO_3 = 399;
+
+// se define la tasa de IVA como una constante
+
+const IVA_TASA = 0.016;
+
+// función para calcular el costo total
+
+function calcularCostoTotal(productosSeleccionados) {
+    let costoTotal = 0;
+
+// Suponemos que el número máximo de productos seleccionados es 5
+    for (let i = 0; i < 5; i++) {
+        let producto = productosSeleccionados[i];
+
+        // Usamos switch para determinar el precio del producto
+        switch (producto) {
+            case 1:
+                costoTotal += PRECIO_PRODUCTO_1;
+                break;
+            case 2:
+                costoTotal += PRECIO_PRODUCTO_2;
+                break;
+            case 3:
+                costoTotal += PRECIO_PRODUCTO_3;
+                break;
+        }
+    }
+
+    let iva = costoTotal * IVA_TASA;
+    let costoTotalConIVA = costoTotal + iva;
+
+    return costoTotalConIVA;
+
+}
+
+// ejemplo de uso: tuvo que leer 
+// sobre los arrays para poner el siguiente ejemplo, 
+// espero haya quedado bien
+
+// Aqui van los porductos que el usuario seleccionó
+let productosSeleccionados = [1,2,3,1]; 
+let totalConIVA = calcularCostoTotal(productosSeleccionados);
+
+// Aquí tuve que investigar sobre la función "toFixed" para redondear el número
+// No supe como realizarlo con lo que vimos en clase
+console.log(`El costo total con IVA es: $${totalConIVA.toFixed(3)}` + " pesos mexicanos" + ".");
